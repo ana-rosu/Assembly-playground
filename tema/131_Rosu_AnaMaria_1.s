30e806509518a0c6fa7902fd3944390c48c5ce78
AnaRosu <anarosu766@gmail.com>
2023-01-08 01:30:19 +0200

    final version

--- tema/131_Rosu_AnaMaria_1.s
+++ tema/131_Rosu_AnaMaria_1.s
@@ -2,22 +2,20 @@
 cerinta: .space 4
 nodes: .space 4
 nr_de_muchii: .space 4
-v: .space 400
+v: .space 4
+dimensiuneV: .space 4
 index: .space 4
 nod_i: .space 4
 nod_j: .space 4
-matrice: .space 40000
-mres: .space 40000
-mcopy: .space 40000
-i: .space 4
-j: .space 4
+matrice: .space 4
+mcopy: .space 4
+mres: .space 4
+dimensiune: .space 4
 k: .space 4
 s: .space 4
 d: .space 4
-formatPrintf1: .asciz "%ld "
-formatPrintf2: .asciz "%ld"
+formatPrintf: .asciz "%ld"
 formatScanf: .asciz "%ld"
-newLine: .asciz "\n"
 .text
 .global main
 // matrix_copy(m, rez, n)
@@ -76,7 +74,7 @@ xorl %edx, %edx
 for2:
   cmp %edx, 20(%ebp)
   je cont_for1
-  
+  // s=0
   movl $0, -12(%ebp)
   xorl %ebx, %ebx
   for3:
@@ -133,13 +131,15 @@ cont_for1:
 incl %ecx
 jmp for1
 continua:
-// dezaloc spatiul folosit pe post de variabile locale in procedura
-addl $12, %esp
 popl %ebx
 popl %edi
 popl %esi
+// dezaloc spatiul folosit pe post de variabile locale in procedura
+addl $12, %esp
 popl %ebp
 ret
+
+
 
 main:
 //citim numarul cerintei
@@ -149,12 +149,8 @@ call scanf
 addl $8, %esp
 //validare cerinta
 movl cerinta, %ebx
-cmp $1, %ebx
+cmp $3, %ebx
 je valid
-
-cmp $2, %ebx
-je valid
-
 jmp et_exit
 valid:
 //citim numarul de noduri
@@ -162,6 +158,51 @@ pushl $nodes
 pushl $formatScanf
 call scanf
 addl $8, %esp
+
+//calculez dimensiunea vectorului in bytes -> n long-uri
+movl $4, %eax
+xorl %edx, %edx
+mull nodes
+movl %eax, dimensiuneV
+
+// aloc spatiul pentru vectorul folosit dinamic
+movl $192, %eax
+movl $0, %ebx
+movl dimensiuneV, %ecx   
+movl $0x3, %edx          
+movl $0x22, %esi         
+movl $-1, %edi           
+movl $0, %ebp
+int $0x80
+
+movl %eax, v
+
+//calculez dimensiunea matricii in bytes
+// %eax = n * n * 4 ->  n * n long-uri
+movl nodes, %eax
+xorl %edx, %edx
+mull nodes
+movl $4, %ebx
+mull %ebx
+movl %eax, dimensiune
+
+// 192 este codul pentru apelul de sistem mmap2
+movl $192, %eax
+// pun valoarea NULL pt a lasa kernelul sa aleaga adresa la care sa creeze maparea in asa fel incat sa nu intre in conflict cu alte mapari existente
+movl $0, %ebx
+// pun dimensiune = n * n * 4, reprezentand numarul de bytes pe care sa ii mapeze
+movl dimensiune, %ecx
+// pun 0x3 -> codul pentru a permite acces read-write: folosesc inclusive-or PROT_READ | PROT_WRITE
+movl $0x3, %edx
+// specific natura maparii: MAP_PRIVATE | MAP_ANON -> pentru a aloca o zona distincta de memorie al carei continut este initializat cu 0, fara a fi conectata la un fisier -> in acest fel obtin PRIVATE ANONYMOUS MAPPING
+movl $0x22, %esi
+// din moment ce am folosit MAP_ANON, este ignorat argumentul fd (file descriptor), insa prin conventie, pun -1
+movl $-1, %edi
+// din nou, pentru ca am folosit MAP_ANON, argumentul offset este ignorat, trebuie sa fie 0
+movl $0, %ebp
+int $0x80
+
+movl %eax, matrice
 
 //citim numarul de legaturi pt fiecare nod si le stocam intr-un vector
 xorl %ecx, %ecx
@@ -176,7 +217,7 @@ call scanf
 addl $8, %esp
 popa
 
-lea v, %esi
+movl v, %esi
 movl nr_de_muchii, %ebx
 movl %ebx, (%esi, %ecx, 4)
 
@@ -188,9 +229,9 @@ movl $0, index
 for_muchii:
 movl index, %ecx
 cmp %ecx, nodes
-je cerinte
+je cerinta3
 
-lea v, %esi
+movl v, %esi
 movl (%esi, %ecx, 4), %ebx
 cmp $0, %ebx
 je pass
@@ -209,7 +250,7 @@ movl %ebx, %ecx
 	  mull nodes
 	  addl nod_j, %eax
         
-	  lea matrice, %edi
+          movl matrice, %edi
 	  movl $1, (%edi, %eax, 4)
  	  
 	  loop for_legaturi
@@ -217,80 +258,57 @@ pass:
 incl index
 jmp for_muchii
 
-//salt la etichete in functie de cerinta
-cerinte:
-movl $1, %ebx
-cmp %ebx, cerinta
-je cerinta1
-
-movl $2, %ebx
-cmp %ebx, cerinta
-je cerinta2
-
-cerinta1:
-// afisare matrice
-movl $0, i
-  for_i:
-    movl i, %ecx
-    cmp %ecx, nodes
-    je et_exit
-
-    movl $0, j
-    for_j:
-	movl j, %ecx
-	cmp %ecx, nodes
-	je cont_af
-	
-	movl i, %eax
-	xorl %edx, %edx
-	mull nodes
-	addl j, %eax
-
-	lea matrice, %edi
-	movl (%edi, %eax, 4), %ebx
-
-	pushl %ebx
-	pushl $formatPrintf1
-	call printf
-	addl $8, %esp
-
-	pushl $0
-	call fflush
-	addl $4, %esp
-
-	incl j
-	jmp for_j
-  cont_af:
-    pusha
-    pushl $newLine
-    call printf
-    addl $4, %esp
-    popa 
-    
-    incl i
-    jmp for_i
-jmp et_exit
-
-cerinta2:
+cerinta3:
 //citim lungimea k a drumului
+pusha
 pushl $k
 pushl $formatScanf
 call scanf
 addl $8, %esp
+popa
 //citim nodul sursa
+pusha
 pushl $s
 pushl $formatScanf
 call scanf
 addl $8, %esp
+popa
 //citim nodul destinatie
+pusha
 pushl $d
 pushl $formatScanf
 call scanf
 addl $8, %esp
+popa
+//aloc dinamic inca 2 matrici
+//mcopy
+movl $192, %eax
+movl $0, %ebx
+movl dimensiune, %ecx   
+movl $0x3, %edx          
+movl $0x22, %esi         
+movl $-1, %edi           
+movl $0, %ebp
+int $0x80
+
+movl %eax, mcopy
+
+//mres
+movl $192, %eax
+movl $0, %ebx
+movl dimensiune, %ecx    
+movl $0x3, %edx          
+movl $0x22, %esi         
+movl $-1, %edi           
+movl $0, %ebp
+int $0x80
+
+movl %eax, mres
+
 //copiem matricea de adiacenta a grafului
 pushl nodes
-pushl $mcopy
-pushl $matrice
+pushl mcopy
+pushl matrice
 call matrix_copy
 addl $12, %esp
 //calculam matrice**k
@@ -303,15 +321,15 @@ je raspuns
 pushl %ecx
 
 pushl nodes
-pushl $mres
-pushl $mcopy
-pushl $matrice
+pushl mres
+pushl mcopy
+pushl matrice
 call matrix_mult
 addl $16, %esp
 
 pushl nodes
-pushl $mcopy
-pushl $mres
+pushl mcopy
+pushl mres
 call matrix_copy
 addl $12, %esp
 
@@ -320,27 +338,46 @@ popl %ecx
 incl %ecx
 jmp for_k
 raspuns:
+movl %eax, %esi
 //afisam mres[nod_sursa][nod_destinatie]
 movl s, %eax
 xorl %edx, %edx
 mull nodes
 addl d, %eax
 
-lea mres, %esi
+movl mres, %esi
 movl (%esi, %eax, 4), %ebx
 
-pusha
+
 pushl %ebx
-pushl $formatPrintf2
+pushl $formatPrintf
 call printf
 addl $8, %esp
-popa
 
-pusha
 pushl $0
 call fflush
 addl $4, %esp
-popa
+
+unmap:
+movl $91, %eax
+movl v, %ebx
+movl nodes, %ecx
+int $0x80
+  
+movl $91, %eax
+movl matrice, %ebx
+movl dimensiune, %ecx
+int $0x80
+
+movl $91, %eax
+movl mcopy, %ebx
+movl dimensiune, %ecx
+int $0x80
+
+movl $91, %eax
+movl mres, %ebx
+movl dimensiune, %ecx
+int $0x80
 
 et_exit:
 movl $1, %eax
